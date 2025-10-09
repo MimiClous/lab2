2fc6cc1c7240479b095305c55647e5cd382f1b1c
Roman <korronaroman@gmail.com>
2025-10-09 22:45:07 +0300

    noo

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE_FILE="main.c"
+OUTPUT_NAME="program"
+
+
+
+gcc $SOURCE_FILE -o $OUTPUT_NAME
+echo "Запуск программы"
+./$OUTPUT_NAME
+rm -rf  $OUTPUT_NAME
--- main.c
+++ main.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
-#include <ctype.h>
-
-int getTwonums(){
-      bool numCheck = True;
-      char user_data1, user_data2;
-      int N;
-      int M;
-
-
-      while (numCheck == True){
-            printf("Enter numbers N and M (Example: 0 500 )");
-            scanf(" %c %c", &user_data, &user_data2);
-            if (isdigit(user_data1) && isdigit(user_data2)){
-                  N = user_data1;
-                  M = user_data2;
-                  numCheck == False
-            }
-            else{
-                  printf("Entered data must be numbers!");
-            }
-      }
-
-      return N, M; 
-}
-
-
-
 
 int main(void){
-      int n, m = getTwonums();
-      printf("%i", n);
-      printf("%i", m);
-	return 0;
+      int n, m;
+
+      printf("Enter numbers N and M (example '0 500')\n");
+      int i_n = scanf(" %d %d", n, m)
+      if (i_n != 2){
+            printf("Entered n and m must be numbers\n");
+      }
+      else{
+            
+      }
+      
+      
+    
+    return 0;
+      
 }
